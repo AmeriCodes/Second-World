28d05291477bbade4f3a4e9ead3cb37d4ee4aa5b
Mateus Américo <mateus-americo@live.com>
2023-04-16 08:16:50 -0300

    primeiro commit

---
+++ Exercícios/Ex036.py
@@ -0,0 +1,3 @@
+# Escreva um programa para o empréstimo bancário de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
