c065ac5d6f4ad6d6123e071ae1105492362b98cb
Mateus Américo <mateus-americo@live.com>
2023-04-16 10:28:41 -0300

    Criação de uma nova branch e adição de alguns comentários no Ex036.py

--- Exercícios/Ex036.py
+++ Exercícios/Ex036.py
@@ -16,10 +16,10 @@ prestação_máxima = (salário * 30) / 100
 if prestação_mensal > prestação_máxima:
     print('Infelizmente você não pode financiar esse imóvel.')
     print(f'A prestação mensal ficaria de R${prestação_mensal:.2f}, ultrapassando R${prestação_máxima:.2f} que seria 30% do seu salário.')
-    sugestão = valor_casa // prestação_máxima
-    possibilidade_em_anos = sugestão // 12
+    sugestão = valor_casa // prestação_máxima # O valor da casa dividido pela prestação máxima, teremos a quantidade de messes adequada.
+    possibilidade_em_anos = sugestão // 12 # Conversão para anos
     print(f'Para financiar uma casa no valor R${valor_casa:.2f} com o seu salário. \nSeria necessário aumentar o tempo de quitação para {possibilidade_em_anos:.0f} anos.')
-    nova_prestação_mensal = valor_casa // sugestão
+    nova_prestação_mensal = valor_casa // sugestão # Uma prestação possível
     print(f'O qual redusiria a dívida mensal de R${prestação_mensal:.2f} para {nova_prestação_mensal:.2f}')
 else:
     print('Este financiamento pode ser realizado.')
