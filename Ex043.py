7fc5e81c239c668a13cebe595d5a139922bdd49f
Mateus Américo <mateus-americo@live.com>
2023-04-19 15:25:18 -0300

    Execução do exercício 43

--- Ex043.py
+++ Ex043.py
@@ -5,3 +5,20 @@
 # 25 até 30: Sobrepeso
 # 30 até 40: Obesidade
 # Acima de 40: Obesidade mórbida
+
+
+peso = float(input('Digite o seu peso: (Kg) '))
+altura = float(input('Digite sua altura: (m) '))
+
+imc = peso / (altura ** 2)
+print(f'Seu IMC foi calculado em {imc:.1f}')
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO')
+elif imc < 25:
+    print('Você está no PESO IDEAL.')
+elif imc < 30:
+    print('Você está com SOBRE PESO.')
+elif imc < 40:
+    print('Você está OBESO, cuide-se!')
+else:
+    print('Você está com OBESIDADE MORBINA, emagreça.')
