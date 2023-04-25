d92463637a33d871030319bb91b4c9d0b2cfb36a
Mateus Américo <mateus-americo@live.com>
2023-04-25 13:42:25 -0300

    Criando os arquivos dos exercícios da branch 8, não estão resolvidos.

---
+++ Exercícios/Ex058.py
@@ -0,0 +1,1 @@
+# Melhore o jogo do exercício 28 onde o computador vai 'pensar' em um número entra 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
