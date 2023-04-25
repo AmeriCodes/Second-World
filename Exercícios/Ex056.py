966410fde93866a96476e193722465d50191cdd5
Mateus Américo <mateus-americo@live.com>
2023-04-25 11:44:49 -0300

    Terminando o Ex56

--- Exercícios/Ex056.py
+++ Exercícios/Ex056.py
@@ -4,10 +4,11 @@
 # Quantas mulheres tem menos de 20 anos.
 
 
-count = 0 # Uso essa variável para contar as idades depois tirar a média.
+count = 0 # Uso essa variável para contar as idades e depois tirar a média.
 average = 0 # Uso essa variável para tirar a média.
-older_man = 0 # 
-older_name = ''
+older_man = 0 # Uso essa variável para achar o homem mais velho.
+older_name = '' # Essa variável será para dizer o nome do homem mais velho.
+count_woman = 0 # Variável que usarei para contar as mulheres sub-20.
 for peaple in range(1,5):
     print(f'===== {peaple}ª PESSOA =====')
     name = str(input('NOME: ')).strip()
@@ -20,6 +21,14 @@ for peaple in range(1,5):
     if sex in 'Mm' and years_old > older_man:
         older_man = years_old
         older_name = name
+    if sex in 'Ff' and years_old < 20:
+        count_woman = count_woman + 1
 average = count / 4
-print(f'A média de idade do grupo é de {average}')
+print(f'A média de idade do grupo é de {average:.1f}')
 print(f'O homem mais velho tem {older_man} anos e se chama {older_name}.')
+if count_woman == 0:
+    print('Não temos mulheres abaixo de 20 anos.')
+elif count_woman == 1:
+    print('Ao todo temos uma mulher abaixo de 20 anos.')
+else:
+    print(f'Ao todo são {count_woman} mulheres abaixo de 20 anos.')
