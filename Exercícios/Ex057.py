e3231479127720246f26b2a071d53e9686da7fcd
Mateus Américo <mateus-americo@live.com>
2023-04-26 10:22:49 -0300

    modificação no ex 57, adição de comentários

--- Exercícios/Ex057.py
+++ Exercícios/Ex057.py
@@ -1,7 +1,9 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 
-sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0] ###
 while sexo not in 'MF':
-    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0] ###
 print(f'Sexo {sexo} resgistrado com sucesso.')
+
+# Esse fatiamento evita que a pessoa digite o sexo por extenso e o programa não reconheça, desse jeito, caso seja digitado 'masculido' o programa jogara para maiusculo e só pegará a primeira letra, funcionando com se tivesse sido digitado só 'M'.
