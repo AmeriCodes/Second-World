9fc86ee08220a0c98087fd9374c34b23f6bfde9f
Mateus Américo <mateus-americo@live.com>
2023-05-03 15:09:05 -0300

    Adição da resposa do professor

--- Exercícios/Ex065.py
+++ Exercícios/Ex065.py
@@ -21,3 +21,25 @@ while question in 'Ss':
         smaller = new_value
 print(f'Você digitou {count} números e a média foi {average:.2f}')
 print(f'O maior valor foi {bigger} e o menor foi {smaller}')
+
+
+# Está foi a resposta do professor
+"""
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} números e a média foi {media}')
+print(f'O maior valor foi {maior} e o menor foi {menor}')
+"""
