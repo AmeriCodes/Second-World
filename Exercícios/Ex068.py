bc249a99ec95a472c0f2c22a26072b0fadf6c0a4
Mateus Américo <mateus-americo@live.com>
2023-05-04 16:08:29 -0300

    Conclusão do exercício 68(complicadinho)

--- Exercícios/Ex068.py
+++ Exercícios/Ex068.py
@@ -7,8 +7,10 @@ print('VAMOS JOGAR PAR OU ÍMPAR')
 count = -1
 while True:
     print('=-' * 12)
+    choise = ' '
     value = int(input('Diga um valor: '))
-    choise = str(input('Par ou Ímpar? [P/I] ')).strip().upper()
+    while choise not in 'PI':
+        choise = str(input('Par ou Ímpar? [P/I] ')).strip().upper()
     pc = randint(0, 10)
     sum = pc + value
     count += 1
@@ -32,3 +34,35 @@ while True:
         else:
             print(f'GAME OVER! Você venceu {count} vezes.')
             break
+
+
+# A resolução do professor foi.
+"""
+from random import randint
+vitoria = 0
+while True:
+    jogador = int(input('Diga um valor: '))
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0] # Esse tipo de validação me interessou, vou transpor isso para o meu código
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total} ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            vitoria += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            vitoria += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos jogar novamente. . .')
+print(f'GAME OVER! Você venceu {vitoria} vezes.')
+"""
